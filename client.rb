95e8c5272043a4f1416f6205b62930faf7cf97b3
Zac Stewart <zgstewart@gmail.com>
2015-04-01 21:13:24 -0400

    Add beginnings of a Gmail gem

---
+++ client.rb
@@ -0,0 +1,26 @@
+require 'faraday'
+
+module Gmail
+  class Client
+    def initialize(token, username: 'me')
+      @username = username
+      @token = token
+      @connection = Faraday.new('https://www.googleapis.com/gmail/v1/users/me') do |conn|
+        conn.request :oauth2, token
+        conn.request :json
+        conn.response :json, :content_type => /\bjson$/
+        conn.use :instrumentation
+        conn.adapter Faraday.default_adapter
+      end
+    end
+
+    def messages(query: '')
+      return enum_for(__method__) unless block_given?
+
+      response = @connection.get('messages', q: query)
+      response.body.fetch('messages').lazy.each do |ref|
+        yield Message.new(@connection.get("messages/#{ref['id']}").body)
+      end
+    end
+  end
+end
---
+++ message.rb
@@ -0,0 +1,38 @@
+module Gmail
+  class Message
+    attr_reader :id, :subject, :from, :body
+
+    def initialize(representation)
+      @id = representation.fetch('id')
+      @representation = representation
+    end
+
+    def subject
+      header = @representation.
+        fetch('payload').
+        fetch('headers').
+        find { |h| h['name'] == 'Subject' } and
+        header.fetch('value')
+    rescue KeyError
+      ''
+    end
+
+    def from
+      header = @representation.
+        fetch('payload').
+        fetch('headers').
+        find { |h| h['name'] == 'From' } and
+        header.fetch('value')
+    rescue KeyError
+      ''
+    end
+
+    def body
+      @representation.
+        fetch('payload').
+        fetch('body')
+    rescue KeyError
+      ''
+    end
+  end
+end
